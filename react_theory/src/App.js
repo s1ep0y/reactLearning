ec2d4dedcf69535a291d7578236f925001a4dac4
s1ep0y <danmol04@gmail.com>
2019-12-09 03:06:36 +0300

    array control

--- react_theory/src/App.js
+++ react_theory/src/App.js
@@ -1,54 +1,90 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Car from './car/car'
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      cars: [
-        {name: 'Ford', year: 2011},
-        {name: 'Audi', year: 2016},
-        {name: 'Mazda', year: 2010}
-      ],
-      pageTitle: "React components"
+    constructor(props) {
+        super(props);
+        this.state = {
+            cars: [
+                {
+                    name: 'Ford',
+                    year: 2011
+                }, {
+                    name: 'Audi',
+                    year: 2016
+                }, {
+                    name: 'Mazda',
+                    year: 2010
+                }
+            ],
+            pageTitle: "React components",
+            showCars: true
+        }
     }
-  }
-  changeTitle = (newTitle) =>{
-    
-    this.setState({
-      pageTitle: newTitle
-    }) 
-  }
+    changeTitle = (newTitle) => {
 
-  inputListen = (event) =>{
-    this.setState({
-      pageTitle: event.target.value
-    })
-  }
-
-  render() {
-    const divStyle = {
-      textAlign: 'center'
+        this.setState({pageTitle: newTitle})
     }
 
-    const cars = this.state.cars  
+    // inputListen = (event) =>{   this.setState({     pageTitle: event.target.value
+    // }) }
 
-    return (
-      <div style={divStyle}>
-      <h1>{this.state.pageTitle}</h1>
-      
-      <input type="text" onChange={this.inputListen}></input>
+    toggleCars = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        })
+    }
 
-      <button onClick={this.changeTitle.bind(this, 'Changed 1st btn')}>Change title</button>
-        {cars.map((info)=> 
-        <Car name={info.name} 
-        year = {info.year} 
-        onChangeTitle={this.changeTitle.bind(this, info.name)}
-        />)}
-      </div>
-    );
-  }
+    onChangeName = (name, index) =>{
+      const car = this.state.cars[index]
+      car.name = name
+      const carsArr = [...this.state.cars]
+      carsArr[index] = car
+      this.setState({
+        carsArr
+      })
+    }
+
+    deleteCar(index){
+      const cars = this.state.cars.concat()
+      cars.splice(index, 1)
+      this.setState({
+        cars
+      })
+    }
+
+    render() {
+        const divStyle = {
+            textAlign: 'center'
+        }
+
+        const cars = this.state.cars
+ 
+        return (
+            <div style={divStyle}>
+                <h1>{this.state.pageTitle}</h1>
+
+                {/* <input type="text" onChange={this.inputListen}></input> */}
+
+                <button onClick={this.toggleCars}>Toggle cars</button>
+
+                { this.state.showCars ?
+                    (cars.map(
+                        (info, index) => <Car
+                            key={index}
+                            name={info.name}
+                            year={info.year}
+                            onChangeName={event=>this.onChangeName(event.target.value, index)}
+                            deleteCar={this.deleteCar.bind(this, index)}
+                            />
+                    ))
+                    :null
+                }
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- react_theory/src/car/car.js
+++ react_theory/src/car/car.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 export default (props) => (
-<div>
+<div style={{
+    border: '1px solid black',
+    marginBottom: '10px',
+    padding: '15px'
+}}>
     <h2>Car name: {props.name}</h2>
     <p>Year <strong>{props.year}</strong></p>
     <p style={{color: 'red'}}>{props.children}</p>
-    <button onClick={props.onChangeTitle}>Click</button>
+    <input type="Text" onChange={props.onChangeName} value={props.name}></input>
+    <button onClick={props.deleteCar}>Delete this line</button>
 </div>
 )
